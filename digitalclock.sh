2ddd378b6e2e969411947813976f656cb8494cba
RinkeshMore <129475029+RinkeshMore@users.noreply.github.com>
2023-04-01 02:20:10 +0530

    shell script on digital clock

---
+++ digitalclock.sh
@@ -0,0 +1,16 @@
+BIRed=$'\e[1;91m'
+Blue=$'\e[0;34m'
+Yellow=$'\e[0;33'
+while true
+do
+   	clear
+	echo -e "\n\n\n\n\n\n\n\n\n\n"
+	echo  "                           *-**-**-**-**-**-**-**-**-**-**-**-**-**"
+ 	echo "                            *         "$BIRed $(date +%T ) "           *"
+	echo  "                           **-**-**-**-**-**-**-**-**-**-**-**-**-*"
+	sleep 1s
+done
+ echo $Yellow $(date +%T)
+echo $Blue $(date +%T)
+ 
+
